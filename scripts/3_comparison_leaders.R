1b3eed34491d4304ae54caa277917811878a1b13
martinoli-f <francescamartinoli@yahoo.com>
2022-12-22 00:14:58 +0100

    plot for comparison - surnames. try another comparison using also the key "pd" - doesn't change anything.

--- scripts/3_comparison_leaders.R
+++ scripts/3_comparison_leaders.R
@@ -1,4 +1,4 @@
-#comparison between different surname of the party leaders
+#comparison between different surname of the party leaders----
 
 library(gtrendsR)
 library(rio)
@@ -39,3 +39,47 @@ party_leaders2 <-  gtrends(
 
 data_leader1 <- as.data.frame(party_leaders$interest_over_time)
 data_leader2 <- as.data.frame(party_leaders2$interest_over_time)
+
+#merge the two dataset usign the date as a key
+library(dplyr)
+merge_data <- bind_rows(data_leader1, data_leader2)
+
+#recoding hits into a numeric variable
+
+merge_data$hits_score <- as.numeric(merge_data$hits) 
+
+#plot for all party leaders: surname
+
+leaders_plot <- ggplot(merge_data, aes(y = hits_score, x = date, group = keyword, color=keyword)) +
+  geom_line(size=1) +
+  theme_bw()
+leaders_plot
+
+#adding "pd" for letta----
+
+pd <- gtrends(
+  keyword = c("meloni", "pd"),
+  geo = "IT",
+  time = "2022-07-21 2022-09-25",
+  gprop = c("web"),
+  hl = "it",
+  compared_breakdown = FALSE,
+  low_search_volume = FALSE,
+  cookie_url = "http://trends.google.com/Cookies/NID",
+  onlyInterest = FALSE
+)
+
+data_pd <- as.data.frame(pd$interest_over_time)
+
+merge_dataPD <- bind_rows(merge_data, data_pd)
+
+merge_dataPD$hits_score <- as.numeric(merge_dataPD$hits)
+
+#plot with surnames + "pd"
+
+leaders_plot2 <- ggplot(merge_dataPD, aes(y = hits_score, x = date, group = keyword, color=keyword)) +
+  geom_line(size=1) +
+  theme_bw()
+leaders_plot2
+
+#doesn't change anything
